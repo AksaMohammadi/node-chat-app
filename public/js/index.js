a67c2bf0e0df381bab6d37ca348403f81f96e0af
Aksa Mohammadi <aksa0004@gmail.com>
2018-11-06 17:03:35 +0530

    add css for chat phase

--- public/js/index.js
+++ public/js/index.js
@@ -17,21 +17,49 @@ var socket = io();
 			jQuery('#messages').append(li);	
 		})
 
+		
+		socket.on('newLocationMessage',function(message) {
+			// console.log('newMessage',message);
+			var li = jQuery('<li></li>');
+			var a= jQuery('<a target ="_blank">My current location</a');
+				li.text(`${message.from}:`)
+				a.attr('href',message.url)
+				li.append(a);
+			jQuery('#messages').append(li);	
+		})		
 
-		// socket.emit('createMessage',{
-		// 	from : 'aqsa',
-		// 	text : 'hello'
-		// }, function(data){
-		// 	console.log('got it',data)
-		// })
+		
 
 		jQuery('#message-form').on('submit',function(e){
 			e.preventDefault();
 
+			var messageTextbox=jQuery('[name=message]')
+
 		socket.emit('createMessage',{
 			from : 'User',
-			text : jQuery('[name=message]').val()
+			text : messageTextbox.val()
 		},function(){
-
+			messageTextbox.val('')
 		})	
 		})
+
+		var locationButton = jQuery('#send-location')
+		locationButton.on('click',function(){
+			if(!navigator.geolocation){
+				return alert('geolocation not supported by your brrowser')
+			}
+
+
+		locationButton.attr('disabled','disabled').text('sending location...')
+			
+			navigator.geolocation.getCurrentPosition(function(position){
+				locationButton.removeAttr('disabled').text('send location')
+				socket.emit('createLocationMessage',{
+					latitude:position.coords.latitude,
+					longitude:position.coords.longitude
+				})
+			},function(){
+				locationButton.removeAttr('disabled').text('send location')
+				alert('unable to fetch data for location');
+			});
+	});
--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const path= require('path');
 const express = require('express')
 const http = require ('http')
 const socketIO = require('socket.io')
-const {generateMessage} = require('./utils/message.js');
+const {generateMessage , generateLocationMessagae} = require('./utils/message.js');
 // console.log(__dirname, '/../public')
 
 const publicPath = path.join(__dirname,'../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 // console.log(publicPath)
 const app = express();
 
@@ -15,31 +15,29 @@ var io = socketIO(server)
 app.use(express.static(publicPath))
 
 io.on('connection',(socket)=>{
-	console.log('new connection');
+	console.log('new connection')
 
 socket.emit('newMessage',generateMessage('Admin','welcome to chat app'))	
 	
-socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined')	);
+socket.broadcast.emit('newMessage', generateMessage('Admin', 'new user joined')	)
+
 socket.on('createMessage',(message , callback) => {
 	console.log('createMessage',message)
-io.emit('newMessage',generateMessage(message.from,message.text));
-callback('this string is from server');
+	io.emit('newMessage',generateMessage(message.from,message.text))
+	callback()
+})
 
-// socket.broadcast.emit('newMessage',{
-// 	from : message.from,
-// 	text : message.text,
-// 	createdAt : new Date().getTime()
-// })	
-});
+socket.on('createLocationMessage',(coords)=>{
+	io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude))
+})
+
 socket.on('disconnect',() => {
 
 			console.log('user made disconnection')
-		});
-})
+		})
 
+})
 
 server.listen(port, () => {
 	console.log(`server is now ready to be used at ${port}`)
 })
-
-
